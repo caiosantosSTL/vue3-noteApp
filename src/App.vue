3e515d4dcd59e1abc1642b4373d9220237828e5e
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-07-09 18:35:26 -0500

    init proj note

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorldVue from './components/HelloWorld.vue';
+
+</script>
+
+<template>
+  <header>
+    <h1>***</h1>
+    <div>
+      <HelloWorldVue></HelloWorldVue>
+    </div>
+  </header>
+
+  
+</template>
+
+<style>
+
+
+</style>
